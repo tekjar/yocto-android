a90dddb765b5b5745ec25c9d5e79383656b4c1eb
kteza1 <k.teza1@gmail.com>
2016-04-26 02:09:00 +0530

    Adding ubuntu yocto & android yocto chroot scripts

---
+++ andyoctoch.sh
@@ -0,0 +1,45 @@
+## ***********************************  ##
+#   Yocto boot scripts for Android       #
+## ************************************ ##
+
+perm=$(id|cut -b 5)
+if [ "$perm" != "0" ];then echo "This script requires root! Type: su"; exit; fi
+mount -o remount,rw /dev/block/mmcblk0p5 /system
+export kit=/sdcard/ubuntu
+export bin=/system/bin
+export mnt=/data/local/mnt
+export USER=root
+mkdir $mnt
+export PATH=$bin:/usr/bin:/usr/local/bin:/usr/sbin:/bin:/usr/local/sbin:$PATH
+export TERM=linux
+export HOME=/root
+if [ -b /dev/block/loop255 ]; then
+	echo "Loop device exists"
+else
+	busybox mknod /dev/block/loop255 b 7 255
+fi
+#mount -o loop,noatime -t ext2 $kit/ubuntu.img $mnt
+losetup /dev/block/loop255 $kit/ubuntu.img
+mount -t ext2 /dev/block/loop255 $mnt
+mount -t devpts devpts $mnt/dev/pts
+mount -t proc proc $mnt/proc
+mount -t sysfs sysfs $mnt/sys
+busybox mount -o bind /sdcard $mnt/sdcard
+busybox mount -o bind /sdcard/external_sd $mnt/external_sd
+busybox sysctl -w net.ipv4.ip_forward=1
+echo "nameserver 8.8.8.8" > $mnt/etc/resolv.conf
+echo "nameserver 8.8.4.4" >> $mnt/etc/resolv.conf
+echo "127.0.0.1 localhost" > $mnt/etc/hosts
+echo "Ubuntu is configured with SSH and VNC servers that can be accessed from the IP:"
+ifconfig eth0
+echo " "
+busybox chroot $mnt /root/init.sh
+
+echo "Shutting down Ubuntu ARM"
+umount $mnt/sdcard
+umount $mnt/external_sd
+umount $mnt/dev/pts
+umount $mnt/proc 
+umount $mnt/sys 
+umount $mnt
+losetup -d /dev/block/loop255
---
+++ ubuyoctoch.sh
@@ -0,0 +1,47 @@
+#! /bin/bash
+
+## ***********************************  ##
+#   Yocto boot scripts for Android       #
+## ************************************ ##
+
+perm=$(id|cut -b 5)
+if [ "$perm" != "0" ];then echo "This script requires root! Type: su"; exit; fi
+
+export mnt=yocto-rootfs
+export USER=root
+export PATH=/bin:/usr/bin:/usr/local/bin:/usr/sbin:/bin:/usr/local/sbin:$PATH
+#export HOME=/root
+
+if [ $2 = "start" ]; then
+	echo "Starting yocto chroot environment"
+elif [ $2 = "stop" ]; then
+	echo "Stoping yocto chroot environment"
+	for f in dev/pts dev proc sys run ; do umount $mnt/$f ; done
+	umount $mnt
+	rm -rf yocto-rootfs
+	exit
+else
+	echo "Invalid 2nd arg. Use either 'start' or 'stop'"
+	exit
+fi
+
+mkdir yocto-rootfs
+mount -t ext4 $1 yocto-rootfs
+
+
+#This file is necessary to execute arm chroot in x86_64
+if [ ! -f $mnt/usr/bin/qemu-arm-static ]; then
+	cp /usr/bin/qemu-arm-static $mnt/usr/bin || { echo "No 'qemu-arm-static' found in host. Install it" ; exit 111; }
+fi
+
+#unbind and bind again
+for f in dev dev/pts proc sys run ; do umount $mnt/$f ; done
+for f in dev dev/pts proc sys run ; do mount -o bind /$f $mnt/$f ; done
+
+echo "nameserver 8.8.8.8" > $mnt/etc/resolv.conf
+echo "nameserver 8.8.4.4" >> $mnt/etc/resolv.conf
+echo "127.0.0.1 localhost" > $mnt/etc/hosts
+echo "Ubuntu is configured with SSH and VNC servers that can be accessed from the IP:"
+ifconfig eth0
+echo " "
+chroot $mnt /bin/bash
